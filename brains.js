037f1663608f838ce5ea4fe7790dd23b81bd2092
Emmanuel uma <umaemmanuel62@gmail.com>
2022-12-22 17:33:38 +0100

    done with the form field

--- brains.js
+++ brains.js
@@ -1,4 +1,5 @@
-let searchIcon, searchMenu, ulClose, ul, barIcons, cart, cartIcon, barIcons0, ul01;
+let searchIcon, searchMenu, ulClose, ul, barIcons, cart, cartIcon, barIcons0, ul01, errorMessage, errorBtn, theName, theEmail, theNumber,
+from, theNameDiv, theEmailDiv, theNumberDiv;
 
 searchIcon = document.getElementById('searchIcon');
 searchMenu = document.getElementById('searchMenu');
@@ -9,6 +10,51 @@ barIcons0 = document.getElementById('barIcons0');
 ul01 = document.getElementById('ul01');
 cart = document.getElementById('cart');
 cartIcon = document.getElementById('cartIcon');
+form = document.getElementById('form010');
+errorMessage = document.getElementById('errorMessage');
+errorBtn = document.getElementById('errorBtn');
+theName = document.getElementById('theName');
+theEmail = document.getElementById('theEmail');
+theNumber = document.getElementById('theNumber');
+//div
+theNameDiv = document.getElementById('theNameDiv');
+theEmailDiv = document.getElementById('theEmailDiv');
+theNumberDiv = document.getElementById('theNumberDiv');
+
+
+
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+});
+errorBtn.addEventListener('click', e => {
+    /* let name, email, number, msg, suc;
+    theName.value == '' ? name = 'your name is empty.': name ='';
+    theEmail.value == '' ? email = 'your email is empty.': email ='';
+    theNumber.value == '' ? number = 'your number is empty.': number ='';
+    if(theName.value != '',theEmail.value != '', theNumber.value != '') {
+        msg = 'Successful'
+        errorMessage.style.color= "green";
+    }else {
+        msg = `error => ${name} ${email} ${number}`;
+        errorMessage.style.color= "red";
+    }
+    errorMessage.textContent = msg; */
+
+    let name, email, number, msg, suc;
+    theName.value == '' ? theNameDiv.style.borderColor = "red" : theNameDiv.style.borderColor = "green";
+    theEmail.value == '' ? theEmailDiv.style.borderColor = "red" : theEmailDiv.style.borderColor = "green";
+    theNumber.value == '' ? theNumberDiv.style.borderColor = "red" : theNumberDiv.style.borderColor = "green";
+
+    if(theName.value != '',theEmail.value != '', theNumber.value != '') {
+        theNameDiv.style.borderColor = "green";
+        theEmailDiv.style.borderColor = "green";
+        theNumberDiv.style.borderColor = "green";
+    }
+});
+
+
 
 
 searchIcon.addEventListener('click', e => {
@@ -48,3 +94,10 @@ window.addEventListener('scroll', e => {
     searchMenu.classList.remove('active');
     searchIcon.classList.remove('fa-close');
 });
+
+
+
+
+
+
+
